1a2ef895cf070d9920bd0348ab5e02808b452cd8
aniketstiwari <anikaittiwari91@gmail.com>
2021-06-20 00:00:15 +0530

    Maging state - part 2

--- src/controllers/slideshow_controller.js
+++ src/controllers/slideshow_controller.js
@@ -4,20 +4,27 @@ export default class extends Controller {
   static targets = [ "slide" ]
   static values = { index: Number }
 
-  initialize() {
-  	//console.log(this.indexValue)
-    //console.log(typeof this.indexValue)
-    //this.index = 0
-    this.showCurrentSlide()
-  }
+  // initialize() {
+  // 	//console.log(this.indexValue)
+  //   //console.log(typeof this.indexValue)
+  //   //this.index = 0
+  //   this.showCurrentSlide()
+  // }
 
   next() {
     this.indexValue++
-    this.showCurrentSlide()
+    //this.showCurrentSlide()
   }
 
   previous() {
     this.indexValue--
+    //this.showCurrentSlide()
+  }
+
+  //Stimulus calls the indexValueChanged() method at initialization and in response 
+  //to any change to the data-slideshow-index-value attribute
+
+  indexValueChanged() {
     this.showCurrentSlide()
   }
 
